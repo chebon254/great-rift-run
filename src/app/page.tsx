f8f8229981775e3543ec539be3651826b98b15f0
chebon254 <kelvinchebon90@gmail.com>
2024-12-09 19:08:49 +0300

    programs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
         </div>
 
         {/* Details */}
-        <div className="bg-[#009951]">
+        <div className="bg-[#009951] w-full">
             <div className="mx-auto max-w-7xl py-20">
               <p className="text-center text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] text-[24px] py-4">Iten is situated in the west of Kenya, at an altitude of 8000ft above sea level. Iten is the perfect training environment for long distance runners, and has been referred regularly as,  &ldquo;The Home of Champions.&rdquo;</p>
               <p className="text-center text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] text-[24px] py-4">We at the Great Rift Run seek to provide an exceptional experience in the Great Rift Valley.</p>
@@ -86,6 +86,47 @@ export default function Home() {
               </div>
             </div>
         </div>
+
+        {/* Programs */}
+        <div className="bg-[#009951] w-full">
+          <div className="mx-auto max-w-7xl pt-10 pb-20">
+            <h1 className="text-center mt-8 text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] text-[80px] leading-none">PROGRAM</h1>
+            <p className="text-center my-8 text-[#FFFFFF] font-[family-name:var(--font-roboto-extrabold)] text-[40px] leading-none">THE BEST SPORTING GETAWAY EXPERIENCE</p>
+            <div className="flex items-center justify-evenly my-8">
+              <div className="h-[560px] w-[400px] p-6 relative"  style={{backgroundImage: 'url(/card-one.png)'}}>
+                <div className="text-left absolute bottom-6">
+                  <h2 className="text-left text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] text-[32px] leading-none">RUN WITH LEGENDS</h2>
+                  <p className="text-left text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] my-6 text-[20px] leading-normal">Run and train fully guided by coaches, workouts and gym facilities</p>
+                  <button className="bg-[#EC221F] text-[#FFFFFF] w-[300px] py-4 px-6 mt-4 font-[family-name:var(--font-roboto-extrabold)]">JOIN NOW</button>
+                </div>
+              </div>
+              <div className="h-[560px] w-[400px] p-6 relative"  style={{backgroundImage: 'url(/card-one.png)'}}>
+                <div className="text-left absolute bottom-6">
+                  <h2 className="text-left text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] text-[32px] leading-none">OPEN SEASON COURSE</h2>
+                  <p className="text-left text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] my-6 text-[20px] leading-normal">Represent your country in the great rift running course challenge!</p>
+                  <button className="bg-[#EC221F] text-[#FFFFFF] w-[300px] py-4 px-6 mt-4 font-[family-name:var(--font-roboto-extrabold)]">JOIN NOW</button>
+                </div>
+              </div>
+              <div className="h-[560px] w-[400px] p-6 relative"  style={{backgroundImage: 'url(/card-one.png)'}}>
+                <div className="text-left absolute bottom-6">
+                  <h2 className="text-left text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] text-[32px] leading-none">SCHOOL CAMP</h2>
+                  <p className="text-left text-[#FFFFFF] font-[family-name:var(--font-roboto-bold)] my-6 text-[20px] leading-normal">Premier camps with great views of the Rift Valley, perfect for holiday</p>
+                  <button className="bg-[#EC221F] text-[#FFFFFF] w-[300px] py-4 px-6 mt-4 font-[family-name:var(--font-roboto-extrabold)]">JOIN NOW</button>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-between my-6 py-8 w-full">
+                    <button>
+                      <Image src={'/arrow-left-white.svg'} height={48} width={42} alt="great rift run"/>
+                    </button>
+                    <button>
+                      <Image src={'/arrow-right-white.svg'} height={48} width={42} alt="great rift run"/>
+                    </button>
+                </div>
+          </div>
+        </div>
+
+        {}
     </main>
   );
 }
