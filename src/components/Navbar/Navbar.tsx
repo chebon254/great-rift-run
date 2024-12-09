ce2990d1a086aa12249ba2e7f670d4e1fd0f93fd
chebon254 <kelvinchebon90@gmail.com>
2024-12-09 15:29:30 +0300

    Navbar fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Menu, X } from 'lucide-react'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
