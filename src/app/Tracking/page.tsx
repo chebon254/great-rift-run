bf46fcad938adc6297d2a846298eea106a890535
chebon254 <kelvinchebon90@gmail.com>
2024-12-18 22:51:06 +0300

    Tracking Ui more changes

--- src/app/Tracking/page.tsx
+++ src/app/Tracking/page.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer/Footer";
+import Link from "next/link";
 import React from "react";
 
 const OrderList = () => {
@@ -61,47 +62,52 @@ const OrderList = () => {
 
   return (
     <>
-      {/* Hero */}
-      <div
-        className="h-[30vh] w-full bg-cover bg-center"
-        style={{ backgroundImage: "url(/hero-image.png)" }}
-      >
-        <div className="mx-auto max-w-7xl px-4 h-full flex justify-center items-center">
-          <div className="h-fit text-left w-full px-4 md:px-8">
-            <div className="text-left text-[#FFFFFF] my-10 font-[family-name:var(--font-roboto-bold)] text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-              ORDER TRACKING
+      <div>
+        {/* Hero */}
+        <div
+          className="h-[30vh] w-full bg-cover bg-center"
+          style={{ backgroundImage: "url(/hero-image.png)" }}
+        >
+          <div className="mx-auto max-w-7xl px-4 h-full flex justify-center items-center">
+            <div className="h-fit text-left w-full px-4 md:px-8">
+              <div className="text-left text-[#FFFFFF] my-10 font-[family-name:var(--font-roboto-bold)] text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+                ORDER TRACKING
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="mx-auto max-w-7xl w-full py-10">
-        <div className="p-6 bg-white">
-          <h1 className="text-2xl font-bold mb-4">My orders</h1>
-          <div className="space-y-4">
-            {orders.map((order, index) => (
-              <div key={index} className="border-b pb-4">
-                <div className="flex justify-between items-center">
-                  <div>
-                    <p className="font-semibold">Order ID: {order.id}</p>
-                    <p>Date: {order.date}</p>
-                    <p>Price: {order.price}</p>
-                  </div>
-                  <div className="flex space-x-4 items-center">
-                    <span className={`px-2 py-1 rounded ${order.statusColor}`}>
-                      {order.status}
-                    </span>
-                    <button
-                      className={`px-4 py-2 border rounded ${order.buttonColor}`}
-                    >
-                      {order.buttonLabel}
-                    </button>
-                    <button className="px-4 py-2 border border-gray-300 rounded">
-                      View details
-                    </button>
+        <div className="mx-auto max-w-7xl w-full py-10">
+          <div className="p-6 bg-white">
+            <h1 className="text-2xl font-bold mb-1">My Orders</h1>
+            <p className="mb-6">Below is a list of orders belonging to <strong>Name:</strong> Kelvin Chebon <strong>Email: </strong>kelvinchebon90@gmail.com</p>
+            <div className="space-y-4">
+              {orders.map((order, index) => (
+                <div key={index} className="border-b pb-4">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <p className="font-semibold">Order ID: {order.id}</p>
+                      <p>Date: {order.date}</p>
+                      <p>Price: {order.price}</p>
+                    </div>
+                    <div className="flex space-x-4 items-center">
+                      <span
+                        className={`px-2 py-1 rounded ${order.statusColor}`}
+                      >
+                        {order.status}
+                      </span>
+                      <button
+                        className={`px-4 py-2 border rounded ${order.buttonColor}`}
+                      >
+                        {order.buttonLabel}
+                      </button>
+                      <Link href="/Tracking/OrderDetails" className="px-4 py-2 border border-gray-300 rounded">
+                        View details
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
